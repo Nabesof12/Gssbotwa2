b1c2198e9aff8607b32738506fe97172db1981fb
Nabesof12 <164458129+Nabesof12@users.noreply.github.com>
2024-07-14 16:14:50 +0430

    Update config.js

--- config.js
+++ config.js
@@ -7,11 +7,12 @@ global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
 global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
-global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.ownername = "  😭🧐🤔🤓ƬӇЄ ƝƛƁЄƧƠƑ🧠🤯📚😱
+";
+global.owner = ['994407246274'];
+global.premium = ['994407246274'];
+global.packname = "😭🧐🤔🤓ƬӇЄ ƝƛƁЄƧƠƑ🧠🤯📚😱";
+global.author = "😭🧐🤔🤓ƬӇЄ ƝƛƁЄƧƠƑ🧠🤯📚😱";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
@@ -25,7 +26,7 @@ global.autoreact = false
 global.onlygroup = false
 global.onlypc = false
 global.welcome = true
-global.autoBlock = true
+global.autoBlock = false
  
 /*REPLY MESSAGE*/
 global.mess = {
